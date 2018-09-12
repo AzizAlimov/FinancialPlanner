f71fcc331bd9fa5d18dd161a554c535bf319af5f
AzizAlimov <iamazizalimov@gmail.com>
2018-09-11 19:04:33 -0700

    Getting Started

---
+++ src/ui/MainClass.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class MainClass {
+    public static void main(String[] args) {
+        FirstMethod();
+        SecondMethod();
+    }
+    private static void FirstMethod(){
+        System.out.print("Hello");
+    }
+    private static void SecondMethod() {
+        System.out.print(" World!");
+    }
+}
